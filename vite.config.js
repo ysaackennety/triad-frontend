0e17b3c9eef5f64df7479c48bf18d8864dde8224
Ysaac Kennety <ysaackennety11@gmail.com>
2026-06-23 23:33:02 -0300

    ajusta vite para vercel e github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/triad-frontend/',
+  base: process.env.VERCEL ? '/' : '/triad-frontend/',
   plugins: [react()],
 })
